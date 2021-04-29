7e866d037fdc6a2941def61932c6bc74dccae317
jrijomah <83398430+jrijomah@users.noreply.github.com>
2021-04-29 09:26:33 +0000

    Add files via upload
    Co Authored by, Hosea Ihiabe, 00920118, 300 Level

---
+++ Ikechukwu Ijomah, 00930118, Hosea Ihiabe, 00920118, 300 Level, Android Studio Project/app/build/generated/source/buildConfig/debug/com/infinityandroid/skycalculator/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.infinityandroid.skycalculator;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.infinityandroid.skycalculator";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
